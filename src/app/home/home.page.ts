461761d261270c3fef998e86bccd090707e37584
marikoaminata81 <107038393+marikoaminata81@users.noreply.github.com>
2022-12-15 19:08:06 +0000

    commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+//background: 'rgba(66,185,12, 0.17)',
+  features: any[] = [
+    {id: 1, name: 'Kayes', src: 'assets/image/kayes.jpg', page: '/detailsRegion'},
+    {id: 2, name: 'Region de Kidal', src: 'assets/image/kidal.jpg',  page: '/detailsRegion'},
+    {id: 3, name: 'Region de Sikasso', src: 'assets/image/Sikasso.jpg',  page: '/detailsRegion'},
+    {id: 4, name: 'Region de Kayes', src: 'assets/image/tombouctou.jpg',  page: '/detailsRegion'},
+  ];
+
+  transactions: any[] = [
+    {id: 1, vendor: 'Received from PhonePe', image: '', amount: 1500, time: '3:00PM'},
+    {id: 2, vendor: 'Flaticons', image: '', amount: -1200, time: '4:00PM'}
+  ];
+  slideOpt ={
+    initialSlide: 0,
+ slidesPerView: 1,
+ autoplay:true,
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  }
+  constructor() {}
+
+}
